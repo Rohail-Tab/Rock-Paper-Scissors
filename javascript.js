cbf80fbe2f0eaa484c76ed25d2dbcb333e94b311
Rohail Tabassum <rohailtabassum@gmail.com>
2022-08-30 16:16:48 +0500

    Create a playRound function
    Function allows user to play single round of game & returns Round winner

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
-    const rocks = "Rock";
-    const paper = "Paper";
-    const scissors = "Scissors";
+    const rocks = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
 
     let randomRPS = Math.random();
 
@@ -13,3 +13,38 @@ function getComputerChoice() {
         return scissors;
     }
 }
+
+function playRound (computerSelection, playerSelection) {
+            
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
+    let getPlayerChoice = prompt("Choose one. Rock Paper Scissors", "");
+    
+    if (getPlayerChoice === null) {
+        return "Choose something";
+    }
+    
+    playerSelection= getPlayerChoice.toLowerCase();
+    console.log(playerSelection);
+
+    if (computerSelection === playerSelection) {
+        return ("It is a tie!");
+    } else if (playerSelection === "rock" & computerSelection === "scissors") {
+        return ("You win. Rock beats Scissors");
+    } else if (playerSelection === "rock" & computerSelection === "paper") {
+        return ("You lose. Paper beats Rock");
+    } else if (playerSelection === "paper" & computerSelection === "rock") {
+        return ("You win. Paper beats Rock");
+    } else if (playerSelection === "paper" & computerSelection === "scissors") {
+        return ("You lose. Scissors beat Paper");
+    } else if (playerSelection === "scissors" & computerSelection === "rock") {
+        return ("You lose. Rock beats Scissors");
+    } else if (playerSelection === "scissors" & computerSelection === "paper") {
+        return ("You win. Scissors beat paper");
+    } else {
+        return ("Check your spelling and Choose the right one.")
+    }
+}
+
+alert(playRound());
